a717378ae02a064b71e03bf53e5d1f81b25a0a9c
chartataglance <mariadass@msn.com>
2022-12-10 14:46:53 +0000

    ff

--- moon-phase/example.php
+++ moon-phase/example.php
@@ -3,25 +3,25 @@
 include "moon.php";
 $moon = new moon();
 
-// if ($moon->set("date", date("2022-12-9"))) 
-// {
+if ($moon->set("date", date("2022-12-9"))) 
+{
 
-//     $phase_name = $moon->get_phase_name();
-//     $phase_at = $moon->get_phase_percent();
-//     $visibility = $moon->get_visibility();
-//     $next_newmoon = $moon->get_next_newmoon();
-//     $next_fullmoon = $moon->get_next_fullmoon();
+    $phase_name = $moon->get_phase_name();
+    $phase_at = $moon->get_phase_percent();
+    $visibility = $moon->get_visibility();
+    $next_newmoon = $moon->get_next_newmoon();
+    $next_fullmoon = $moon->get_next_fullmoon();
 
-//     echo 
+    echo 
 
-//     <<<MOON_INFO
-// phase name: {$phase_name} <br>
-// phase percentage: {$phase_at}% <br>
-// visibility: {$visibility}% <br>
-// next new moon: {$next_newmoon} <br>
-// next full moon: {$next_fullmoon} <br>
-// MOON_INFO;
-// }
+    <<<MOON_INFO
+phase name: {$phase_name} <br>
+phase percentage: {$phase_at}% <br>
+visibility: {$visibility}% <br>
+next new moon: {$next_newmoon} <br>
+next full moon: {$next_fullmoon} <br>
+MOON_INFO;
+}
 
  if($moon->set("date", date("2022-12-9"))) 
  {
